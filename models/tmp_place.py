54b826243a926f42cbc2760aeb4c479faaa1331a
Gisa Dan <d.gisa2@alustudent.com>
2026-05-27 11:55:16 +0000

    Fix tmp_ model files to import from tmp_base_model

--- models/tmp_place.py
+++ models/tmp_place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines the Place class"""
-from models.base_model import BaseModel
+from models.tmp_base_model import BaseModel
 
 
 class Place(BaseModel):
--- models/tmp_review.py
+++ models/tmp_review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines the Review class"""
-from models.base_model import BaseModel
+from models.tmp_base_model import BaseModel
 
 
 class Review(BaseModel):
